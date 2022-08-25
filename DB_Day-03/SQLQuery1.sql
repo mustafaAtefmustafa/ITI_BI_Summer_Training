2d8ac3488d7453ba0b3b0d9961f83f9a63617818
mustafaAtefmustafa <mustafaatef761@gmail.com>
2022-08-25 18:45:57 +0200

    DB Day-03

---
+++ DB_Day-03/SQLQuery1.sql
@@ -0,0 +1,69 @@
+--1
+SELECT d.Dnum, d.Dname, e.SSN, e.Fname + ' ' + e.Lname AS "Manager Name"
+FROM Departments d, Employee e
+WHERE d.MGRSSN = e.SSN
+
+--2
+SELECT d.Dname, p.Pname
+FROM Departments d, Project p
+WHERE p.Dnum = d.Dnum
+
+--3
+SELECT de.*, e.Fname + ' ' + e.Lname AS "Dependee Name"
+FROM Dependent de, Employee e
+WHERE de.ESSN = e.SSN
+
+--4
+SELECT Pnumber, Pname, Plocation
+FROM Project
+WHERE City in ('Cairo', 'Alex')
+
+--5
+SELECT *
+FROM Project
+WHERE Pname like 'a%'
+
+--6
+SELECT *
+FROM Employee 
+WHERE Dno = 30 AND Salary BETWEEN 1000 AND 2000
+
+--7
+SELECT e.Fname + ' ' + e.Lname AS "Name"
+FROM Employee e JOIN Works_for w
+ON e.SSN = w.ESSN
+JOIN Project p
+ON w.Pno = p.Pnumber
+WHERE e.Dno = 10 AND w.Hours >= 10 AND p.Pname = 'AL Rabwah'
+
+--8
+SELECT e.Fname + ' ' + e.Lname AS "Name"
+FROM Employee e JOIN Employee s
+ON e.Superssn = s.SSN
+WHERE s.Fname + ' ' + s.Lname = 'Kamel Mohamed'
+
+--9
+SELECT e.Fname + ' ' + e.Lname AS "Name", p.Pname
+FROM Employee e JOIN Works_for w
+ON e.SSN = w.ESSn
+JOIN Project p
+ON w.Pno = p.Pnumber
+ORDER BY p.Pname
+
+--10
+SELECT p.Pnumber, d.Dname, e.Lname, e.Address, e.Bdate
+FROM Project p JOIN Departments d
+ON p.Dnum = d.Dnum
+JOIN Employee e
+ON e.SSN = d.MGRSSN
+WHERE p.City = 'Cairo'
+
+--11
+SELECT e.*
+FROM Employee e JOIN Departments d
+ON e.SSN = d.MGRSSN
+
+--12
+SELECT e.* , de.*
+FROM Employee e LEFT JOIN Dependent de
+ON e.SSN = de.ESSN
